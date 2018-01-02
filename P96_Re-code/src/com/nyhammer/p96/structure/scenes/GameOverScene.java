8a50f39987ed00ccdc14228bee5d523ab735d784
McFlyboy <hsn95@live.com>
2018-01-02 08:13:39 +0100

    GAME OVER
    Added a "Game over" menu which let's the player restart the game, or
    exit the game. Also, going into the pause menu now makes a sound :P

---
+++ P96_Re-code/src/com/nyhammer/p96/structure/scenes/GameOverScene.java
@@ -0,0 +1,101 @@
+package com.nyhammer.p96.structure.scenes;
+
+import com.nyhammer.p96.audio.Music;
+import com.nyhammer.p96.entities.TextField;
+import com.nyhammer.p96.graphics.Render;
+import com.nyhammer.p96.structure.Menu;
+import com.nyhammer.p96.structure.ResourceStorage;
+import com.nyhammer.p96.structure.Scene;
+import com.nyhammer.p96.structure.controlSchemes.MenuControls;
+import com.nyhammer.p96.ui.GameWindow;
+import com.nyhammer.p96.util.math.vector.Vector2f;
+import com.nyhammer.p96.util.timing.Timer;
+
+public class GameOverScene extends Scene{
+	private MenuControls controls;
+	private Menu menu;
+	private TextField titleText;
+	public GameOverScene(Timer timer){
+		super(timer);
+		controls = new MenuControls();
+		Music bgm = new Music("P96_GAME_OVER.ogg");
+		bgm.setVolume(0f);
+		ResourceStorage.add("gameOverBGM", bgm);
+		menu = new Menu(this.timer, "Retry", "Exit");
+		menu.setPosition(new Vector2f(0f, -0.225f));
+		titleText = new TextField();
+		titleText.scale = new Vector2f(0.007f, 0.007f);
+		titleText.setText("-GAME OVER-");
+		titleText.position.y = 0.1f;
+	}
+	@Override
+	protected void startSpecifics(){
+		ResourceStorage.getMusic("gameOverBGM").play();
+	}
+	@Override
+	protected void updateSpecifics(float deltaTime){
+		Music bgm = ResourceStorage.getMusic("gameOverBGM");
+		float volume = bgm.getVolume();
+		if(volume < 1f){
+			volume += 0.8f * deltaTime;
+			if(volume > 1f){
+				volume = 1f;
+			}
+			bgm.setVolume(volume);
+		}
+		bgm.update();
+		updateControls();
+	}
+	@Override
+	protected void renderSpecifics(){
+		Render.addToQueue(titleText);
+		menu.render();
+	}
+	@Override
+	protected void stopSpecifics(){
+		ResourceStorage.getMusic("gameOverBGM").stop();
+		menu.reset();
+	}
+	@Override
+	protected void disposeSpecifics(){
+		ResourceStorage.disposeMusic("gameOverBGM");
+	}
+	private void updateControls(){
+		if(menu.isOptionSelected()){
+			return;
+		}
+		if(controls.isPressed(controls.getConfirm())){
+			menu.setOptionSelected(true);
+			ResourceStorage.getSound("confirmSound").play();
+		}
+		if(controls.isPressed(controls.getCancel())){
+			
+		}
+		if(controls.isPressed(controls.getLeft())){
+			
+		}
+		if(controls.isPressed(controls.getRight())){
+			
+		}
+		if(controls.isPressed(controls.getUp())){
+			menu.scrollUp();
+		}
+		if(controls.isPressed(controls.getDown())){
+			menu.scrollDown();
+		}
+	}
+	public int checkMenus(){
+		if(menu.isOptionContinue()){
+			menu.setOptionSelected(false);
+			switch(menu.getActiveOption()){
+			case 0:
+				menu.setOptionContinue(false);
+				return 1;
+			default:
+				GameWindow.close();
+				break;
+			}
+		}
+		return 0;
+	}
+}
--- P96_Re-code/src/com/nyhammer/p96/structure/scenes/GlobalScene.java
+++ P96_Re-code/src/com/nyhammer/p96/structure/scenes/GlobalScene.java
@@ -14,8 +14,9 @@ import com.nyhammer.p96.util.timing.Time;
 public class GlobalScene extends Scene{
 	private GlobalControls controls;
 	private PauseScene pauseScene;
+	private GameOverScene gameOverScene;
 	private GameplayScene gameplayScene;
-	private boolean gamePaused;
+	private int gameState;
 	private TextField fpsText;
 	public GlobalScene(){
 		super(null);
@@ -23,51 +24,66 @@ public class GlobalScene extends Scene{
 		fpsText.scale = new Vector2f(0.0015f, 0.0015f);
 		fpsText.mainColor.blue = 0f;
 		fpsText.visible = false;
-		gamePaused = false;
+		gameState = 0;
 		controls = new GlobalControls();
 		ResourceStorage.add("square", Models.createSquare());
 		ResourceStorage.add("optionSound", new Sound("system/option.ogg"));
 		ResourceStorage.add("confirmSound", new Sound("system/confirm.ogg"));
 		ResourceStorage.add("cancelSound", new Sound("system/cancel.ogg"));
-	}
-	@Override
-	protected void startSpecifics(){
 		pauseScene = new PauseScene(this.timer);
+		gameOverScene = new GameOverScene(this.timer);
 		gameplayScene = new GameplayScene(this.timer);
 		gameplayScene.start();
 	}
 	@Override
+	protected void startSpecifics(){
+		
+	}
+	@Override
 	protected void updateSpecifics(float deltaTime){
 		updateControls();
-		if(!gamePaused){
+		if(gameState == 0){
 			gameplayScene.update();
 			if(gameplayScene.isGameOver()){
-				gameplayScene.dispose();
-				gameplayScene = new GameplayScene(this.timer);
-				gameplayScene.start();
+				gameplayScene.stop();
+				gameState = 2;
+				gameOverScene.start();
 			}
 		}
-		else{
+		else if(gameState == 1){
 			pauseScene.update();
 			int menuState = pauseScene.checkMenus();
 			if(menuState == 1 || pauseScene.shouldExit()){
-				gamePaused = false;
+				gameState = 0;
 				updateGameState();
 			}
 			else if(menuState == 2){
-				System.out.println("Test");
-				gamePaused = false;
+				gameState = 0;
 				gameplayScene.dispose();
 				gameplayScene = new GameplayScene(this.timer);
 				updateGameState();
+			}
+		}
+		else{
+			gameOverScene.update();
+			int menuState = gameOverScene.checkMenus();
+			if(menuState == 1){
+				gameState = 0;
+				gameplayScene.dispose();
+				gameplayScene = new GameplayScene(this.timer);
+				gameOverScene.stop();
+				gameplayScene.start();
 			}
 		}
 	}
 	@Override
 	protected void renderSpecifics(){
 		gameplayScene.render();
-		if(gamePaused){
+		if(gameState == 1){
 			pauseScene.render();
+		}
+		else if(gameState == 2){
+			gameOverScene.render();
 		}
 		Render.setScene(this);
 		fpsText.setText("FPS: " + Time.getFPS());
@@ -83,15 +99,20 @@ public class GlobalScene extends Scene{
 	protected void disposeSpecifics(){
 		gameplayScene.dispose();
 		pauseScene.dispose();
+		gameOverScene.dispose();
 		ResourceStorage.disposeModel("square");
 		ResourceStorage.disposeSound("optionSound");
 		ResourceStorage.disposeSound("confirmSound");
 		ResourceStorage.disposeSound("cancelSound");
 	}
 	private void updateControls(){
-		if(controls.isPressed(controls.getPause())){
-			gamePaused = !gamePaused;
+		if(controls.isPressed(controls.getPause()) && gameState < 2){
+			gameState++;
+			gameState %= 2;
 			updateGameState();
+			if(gameState == 1){
+				ResourceStorage.getSound("confirmSound").play();
+			}
 		}
 		if(controls.isPressed(controls.getFPS())){
 			fpsText.visible = !fpsText.visible;
@@ -102,7 +123,7 @@ public class GlobalScene extends Scene{
 		}
 	}
 	private void updateGameState(){
-		if(!gamePaused){
+		if(gameState == 0){
 			pauseScene.stop();
 			gameplayScene.start();
 		}
